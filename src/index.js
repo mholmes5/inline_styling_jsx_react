9415305bd5ad18f3ea47900ea518a7757936b010
Michael Holmes <michaelholmes618@gmail.com>
2021-04-10 15:53:58 -0500

    add inline styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello Sly!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+  backgroundColor: "grey"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello Sly!</h1>,
+  document.getElementById("root")
+);
